a8796ddb6b43ba388efde1ccf9a1246529fd613b
ImOBxx <166965179+ImOBxx@users.noreply.github.com>
2024-09-15 09:13:51 +0530

    Add files via upload

---
+++ Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Abhishek Banerjee on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
